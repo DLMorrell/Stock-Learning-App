595e3e54cf2b538b1da6558a08a545ebd0e4cbf2
Daniel Morrell <danlee5678@gmail.com>
2020-10-03 00:25:51 -0500

    Created a full slice for C#/react project and setup our alpaca API wrappers.
    Setup a simple project to make sure we have the environment set up properly.

---
+++ API/AccountAPI.cs
@@ -0,0 +1,145 @@
+using Alpaca.Markets;
+using System;
+
+namespace Stock_Learning_App.API
+{
+    public class AccountAPI : IAPI
+    {
+        private static Alpaca.Markets.IAccount account;
+
+        public AccountAPI()
+        {
+            accessAccount();
+        }
+
+        private static async void accessAccount()
+        {
+            account = await IAPI.tradingClient.GetAccountAsync();
+        }
+
+        public Guid getAccountId()
+        {
+            return account.AccountId;
+        }
+
+        public string getAccountNumber()
+        {
+            return account.AccountNumber;
+        }
+
+        public decimal getBuyingPower()
+        {
+            return account.BuyingPower;
+        }
+
+        public DateTime getCreatedAtUtc()
+        {
+            return account.CreatedAtUtc;
+        }
+
+        public string getCurrency()
+        {
+            return account.Currency;
+        }
+
+        public long getDayTradeAccount()
+        {
+            return account.DayTradeCount;
+        }
+
+        public decimal getDayTradingBuyingPower()
+        {
+            return account.DayTradingBuyingPower;
+        }
+
+        public decimal getInitialMargin()
+        {
+            return account.InitialMargin;
+        }
+
+        public bool isAccountBlocked()
+        {
+            return account.IsAccountBlocked;
+        }
+
+        public bool isDayPatternTrader()
+        {
+            return account.IsDayPatternTrader;
+        }
+
+        public bool isTradingBlocked()
+        {
+            return account.IsTradingBlocked;
+        }
+
+        public bool isTransfersBlocked()
+        {
+            return account.IsTransfersBlocked;
+        }
+
+        public decimal getLastEquity()
+        {
+            return account.LastEquity;
+        }
+
+        public decimal getLastMaintenanceMargin()
+        {
+            return account.LastMaintenanceMargin;
+        }
+
+        public decimal getLongMarketValue()
+        {
+            return account.LongMarketValue;
+        }
+
+        public decimal getMaintenanceMargin()
+        {
+            return account.MaintenanceMargin;
+        }
+
+        public long getMultiplier()
+        {
+            return account.Multiplier;
+        }
+
+        public decimal getRegulationBuyingPower()
+        {
+            return account.RegulationBuyingPower;
+        }
+
+        public bool isShortingEnabled()
+        {
+            return account.ShortingEnabled;
+        }
+
+        public decimal isShortMarketValue()
+        {
+            return account.ShortMarketValue;
+        }
+
+        public decimal getSma()
+        {
+            return account.Sma;
+        }
+
+        public AccountStatus getStatus()
+        {
+            return account.Status;
+        }
+
+        public decimal getTradeableCash()
+        {
+            return account.TradableCash;
+        }
+
+        public bool isTradeSuspendedByUser()
+        {
+            return account.TradeSuspendedByUser;
+        }
+
+        public decimal getWithdrawableCash()
+        {
+            return account.WithdrawableCash;
+        }
+    }
+}
---
+++ API/PortfolioAPI.cs
@@ -0,0 +1,15 @@
+﻿using Alpaca.Markets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_Learning_App.API
+{
+    public class PortfolioAPI : IAPI
+    {
+        public static async Task<IReadOnlyList<IPosition>> accessPositions()
+        {
+            return await IAPI.tradingClient.ListPositionsAsync();
+        }
+    }
+}
